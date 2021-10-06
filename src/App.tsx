11e8001fcd85ee0d5a7a933fc484a9772b4a4056
jsemalvarez <jsemalvarez@gmail.com>
2021-10-05 21:49:51 -0300

    TS tipos basicos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TiposBasicos } from "./typescript/TiposBasicos"
 
-function App() {
+const App = () => {
+
+  const nombre: string | number = 'Jose'
+  const edad: number = 35
+  let estaActivo: boolean = true
+
+  const poderes = ['Velocidad','Valor','Volar']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-2">
+      <h2>Introduccion a TS - React</h2>
+      <hr />
+      <TiposBasicos />
+      { nombre }, { edad }, { (estaActivo) ? 'true' : 'false' }
+      <br />
+      { poderes.join(', ')}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
