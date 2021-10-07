baa64afef28c89597e1337cf0118d1c8b2b2e9a0
jsemalvarez <jsemalvarez@gmail.com>
2021-10-06 22:04:13 -0300

    funciones, retorno y argumentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { TiposBasicos } from "./typescript/TiposBasicos"
+// import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
 
-import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
+import { Funciones } from "./typescript/Funciones"
 
 const App = () => {
 
@@ -8,7 +9,7 @@ const App = () => {
     <div className="mt-2">
       <h2>Introduccion a TS - React</h2>
       <hr />
-      <ObjetosLiterales />
+      <Funciones />
 
     </div>
   )
---
+++ src/typescript/Funciones.tsx
@@ -0,0 +1,16 @@
+
+
+export const Funciones = () => {
+
+    const sumar = ():number => {
+        return 5
+    }
+
+
+    return (
+        <>
+            <h3>Funciones</h3>
+            <span> El resultado es: { sumar() }</span>
+        </>
+    )
+}
